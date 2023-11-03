a51ac406764e06da55b8b892c97dbdec7472ce06
Samuel Galindo <samuel.galindo@correounivalle.edu.co>
2023-11-02 19:20:10 -0500

    Django rest framework y correción del requirements.txt

--- backend/concesionario/concesionario/settings.py
+++ backend/concesionario/concesionario/settings.py
@@ -38,12 +38,14 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'corsheaders',
+    'rest_framework',
     'concesionarioapp',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -78,12 +80,14 @@ WSGI_APPLICATION = 'concesionario.wsgi.application'
 
 DATABASES = {
     'default': {
-        ## Cada uno ponga su base de datos aqui
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'BdConcesionario',
+        'USER': 'postgres',
+        'PASSWORD': '1234',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
