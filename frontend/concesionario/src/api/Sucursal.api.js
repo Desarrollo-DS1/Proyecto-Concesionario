bd241cba135fff0e38ba254d47acc97880096989
John Freddy <john.freddy.belalcazar@correounivalle.edu.co>
2023-11-08 22:42:32 -0500

    Serializer views y api de sucursal consigurados

--- frontend/concesionario/src/api/Sucursal.api.js
+++ frontend/concesionario/src/api/Sucursal.api.js
@@ -6,4 +6,10 @@ const sucursalApi = axios.create({
 
 export const getAllSucursales = () => sucursalApi.get('/')
 
-export const getSucursal = (id) => sucursalApi.get(`/${id}/`)
+export const getSucursal = (id_sucursal) => sucursalApi.get(`/${id_sucursal}/`)
+
+export const createSucursal = (sucursal) => sucursalApi.post('/', sucursal);
+
+export const updateSucursal = (id_sucursal, sucursal) => sucursalApi.put(`/${id_sucursal}/`, sucursal);
+
+export const deleteSucursal = (id_sucursal) => sucursalApi.delete(`/${id_sucursal}`);
