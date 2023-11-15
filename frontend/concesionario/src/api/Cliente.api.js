8f8f91e65847f577fa66a279d2ca11ff7983d5b8
Christian Vargas <vargas.christian@correounivalle.edu.co>
2023-11-14 23:49:51 -0500

    Solución de bug en envío de token de autenticación desde el front para Clientes

--- frontend/concesionario/src/api/Cliente.api.js
+++ frontend/concesionario/src/api/Cliente.api.js
@@ -1,17 +1,17 @@
 import axios from 'axios';
 
-const clienteApi = axios.create({
-    baseURL: 'http://localhost:8000/concesionarioapp/api/v1/Cliente/',
-    headers: localStorage.getItem('authTokens') ? { Authorization: `Bearer ${JSON.parse(localStorage.getItem('authTokens')).access}` } : null
-});
+const clienteApi = (token) => axios.create({
+    baseURL: 'http://localhost:8000/concesionarioapp/api/v1/Cliente',
+    headers: { Authorization: `Bearer ${token}` }}
+);
 
-export const getAllClientes = () => clienteApi.get('/');
+export const getAllClientes = (token) => clienteApi(token).get('/')
 
-export const getCliente = (id) => clienteApi.get(`/${id}/`);
+export const getCliente = (id, token) => clienteApi(token).get(`/${id}/`)
 
-export const createCliente = (cliente) => clienteApi.post('/', cliente);
+export const createCliente = (cliente, token) => clienteApi(token).post('/', cliente);
 
-export const updateCliente = (id, cliente) => clienteApi.put(`/${id}/`, cliente);
+export const updateCliente = (id, cliente, token) => clienteApi(token).put(`/${id}/`, cliente);
 
-export const deleteCliente = (id) => clienteApi.delete(`/${id}/`);
+export const deleteCliente = (id, token) => clienteApi(token).delete(`/${id}/`);
 
